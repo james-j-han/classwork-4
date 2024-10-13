f78cae6a89aa992053e516155f826bc929e87ce3
James Han <jayimshan@gmail.com>
2024-10-13 04:38:43 -0400

    Exercise 1

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CW 04</title>
+
+        <link rel="stylesheet" href="styles.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="body-container">
+            <div class="left-container"></div>
+            <div class="main-container">
+                <div class="exercise">
+                    <?php
+                        function hello_world() {
+                            return "Hello world!";
+                        }
+                        
+                        echo hello_world();
+                    ?>
+                </div>
+                <div class="exercise">2</div>
+                <div class="exercise">3</div>
+                <div class="exercise">4</div>
+                <div class="exercise">5</div>
+                <div class="exercise">6</div>
+            </div>
+            <div class="right-container"></div>
+        </div>
+    </body>
+</html>
